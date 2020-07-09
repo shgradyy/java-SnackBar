81a8e461db86b556a2ac050acc9284d1a8f97403
Shantay Grady <shantaygrady@gmail.com>
2020-07-09 19:19:52 -0400

    Package and class setup

---
+++ src/snackbarApp/Main.java
@@ -0,0 +1,10 @@
+package snackbarApp;
+
+public class Main {
+	private static void workWithData() {
+		Systemout.println("Welcome to Java Snackbar!");
+	}
+	private static void main(String[] args)  {
+		workWithData();
+	}
+}
